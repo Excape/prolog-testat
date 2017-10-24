fd02977469ee19cdef3ebb980df0f01f12a48cee
Jonas Matter <jonas.matter@bluewin.ch>
2017-10-24 19:44:04 +0200

    convert rows to cols helper predicate

--- sudoku-lib.pl
+++ sudoku-lib.pl
@@ -32,3 +32,27 @@ sudoku(A1,A2,A3,A4,B1,B2,B3,B4,C1,C2,C3,C4,D1,D2,D3,D4) :-
       [C1, C2, D1, D2],
       [C3, C4, D3, D4]
     ]).
+
+append([],L,L).
+append([H|T],L2,[H|L3]) :- append(T,L2,L3).
+
+getFirst([], AccRest, AccRest, Acc, Acc).
+getFirst([[H1|T1]|T2], AccRest, Rest, Acc, R) :- 	append(Acc, [H1], L3),
+    												append(AccRest, [T1], L4),
+    												getFirst(T2, L4, Rest, L3, R).
+
+toCol([[]|_], Acc, Acc).
+toCol(L, Acc, R) :- getFirst(L, [], Rest, [], Col), append(Acc, [Col], L3), toCol(Rest,  L3, R).
+toCol(Rows, Cols) :- toCol(Rows, [], Cols).
+
+/** <examples>
+?- toCol( [[11, 12, 13, 14, 15, 16, 17, 18, 19],
+            [21, 22, 23, 24, 25, 26, 27, 28, 29],
+            [31, 32, 33, 34, 35, 36, 37, 38, 39],
+            [41, 42, 43, 44, 45, 46, 47, 48, 49],
+            [51, 52, 53, 54, 55, 56, 57, 58, 59],
+          	 [61, 62, 63, 64, 65, 66, 67, 68, 69],
+            [71, 72, 73, 74, 75, 76, 77, 78, 79],
+            [81, 82, 83, 84, 85, 86, 87, 88, 89],
+            [91, 92, 93, 94, 95, 96, 97, 98, 99]], X)
+*/
